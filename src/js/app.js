6925687a45d88a1b90635ad00199140ff05bcab9
Mauro Valdez <josenova0@gmail.com>
2022-06-29 20:39:50 -0300

    4.1

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,21 @@ import 'swiper/css/bundle';
 //variables
 
 const header = document.querySelector('#header');
-const navShop = document.querySelector('#cart-shop');
 const navMenu = document.querySelector('#nav-menu');
 const navClose = document.querySelector('#nav-close');
 const navToggle = document.querySelector('#nav-toggle');
 const navLink = document.querySelectorAll('.nav__link');
+const secciones = document.querySelectorAll('section[id]');
+const upScroll = document.querySelector('#scroll-up');
+const cart = document.querySelector('#cart');
+const cartShop = document.querySelector('#cart-shop');
+const cartClose = document.querySelector('#cart-close');
 
-
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', () => {
+  scrollHeader();
+  scrollActivo();
+  scrollUP();
+})
 document.addEventListener('DOMContentLoaded', () => {
   menu();
 })
@@ -32,6 +39,16 @@ function menu() {
       link.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
       })
+    })
+  }
+  if(cartShop) {
+    cartShop.addEventListener('click', () => {
+      cart.classList.add('show-cart');
+    })
+  }
+  if(cartClose) {
+    cartClose.addEventListener('click', () => {
+      cart.classList.remove('show-cart');
     })
   }
 }
@@ -73,3 +90,34 @@ let newSwiper = new Swiper(".new-swiper", {
     },
   },
 });
+
+function scrollActivo() {
+  const scrollY = window.pageYOffset;
+
+  secciones.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 30;
+    const sectionId = section.getAttribute("id");
+    
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav__menu a[href="#${sectionId}"]`)
+        .classList
+        .add('active-link');
+    } else {
+      document
+        .querySelector(`.nav__menu a[href="#${sectionId}"]`)
+        .classList
+        .remove('active-link');
+    }
+  });
+}
+
+function scrollUP() {
+  if(scrollY >= 500) {
+    upScroll.classList.add('show-scroll');
+  }else {
+    upScroll.classList.remove('show-scroll');
+  }
+}
